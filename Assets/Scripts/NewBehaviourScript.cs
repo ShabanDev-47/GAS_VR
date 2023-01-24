818479f7de53847993303ec418736b4b0d9d2d62
CoolDev47 <mohamed.shaban6662@gmail.com>
2023-01-24 22:21:45 +0200

    props

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Git_Test m = gameObject.AddComponent<Git_Test>();
+        
+        Git_Test m = 
+            new Git_Test();
         print(m.Experience);
         m.Experience++;
         m.Experience++;
