aa6516124084dcbfd610101efe5db0424d4b14df
CoolDev47 <mohamed.shaban6662@gmail.com>
2023-01-25 16:17:36 +0200

    interface prac

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : Git_Test
+public class NewBehaviourScript : Git_Test,Iprintable
 {
     
     // Start is called before the first frame update
     void Start()
     {
-
+        
         //Git_Test m = 
         //    new Git_Test();
         //print(m.Experience);
@@ -16,6 +16,7 @@ public class NewBehaviourScript : Git_Test
         //m.Experience++;
         //print(m.Experience);
         Talk();
+        printing();
     }
 
     public override void Talk()
@@ -24,7 +25,10 @@ public class NewBehaviourScript : Git_Test
         print("I love you dad");
     }
 
-
+    public void printing()
+    {
+        print("Shiiit");
+    }
 
 
 }
